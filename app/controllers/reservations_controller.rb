0e5f0b2c0836d6330cf06e19748c46cd7b08c07f
Heart Beat Studio <blacklight728@gmail.com>
2015-09-14 00:03:50 -0400

    Add restaurants model, controller and views

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,34 @@
 class ReservationsController < ApplicationController
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+  def reservation_params
+    
+  end
 end
